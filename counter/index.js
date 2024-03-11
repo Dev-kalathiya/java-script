ecfedde8edc4f0bb73e74421888e477633f152d1
aryansabhani <aryansabhani2660@gmail.com>
2024-03-11 19:54:23 +0530

    count

---
+++ counter/index.js
@@ -0,0 +1,39 @@
+
+
+const show = () =>{
+ let count4 = document.querySelector('#count1').innerHTML;
+ let count = 0
+ let fisrt = setInterval(() =>{
+    count++;
+       document.querySelector('#count1').innerHTML = count
+      if(count4 == count){
+        clearInterval(fisrt)
+        show1();
+      } 
+ },1) 
+}
+const show1 = () =>{
+    let count5 = document.querySelector('#count2').innerHTML;
+    let count = 0
+    let second = setInterval(() =>{
+       count++;
+          document.querySelector('#count2').innerHTML = count
+         if(count5 == count){
+           clearInterval(second)
+           show2();
+         } 
+    },1) 
+   }
+   const show2 = () =>{
+    let count6 = document.querySelector('#count3').innerHTML;
+    let count = 0
+    let third = setInterval(() =>{
+       count++;
+          document.querySelector('#count3').innerHTML = count
+         if(count6 == count){
+           clearInterval(third)
+    
+         } 
+    },1) 
+   }
+   setTimeout(show)
