2f1f2ca901c19c97aad4b59c93a2420644eb9518
aryansabhani <aryansabhani2660@gmail.com>
2024-03-20 19:53:23 +0530

    api

---
+++ API-prec/index.js
@@ -0,0 +1,31 @@
+
+
+const uimaker=(data)=>{
+
+data.map((ele)=>{
+    let title=document.createElement("p")
+    title.innerHTML=ele.title
+    title.setAttribute("id", "title")
+    let price=document.createElement("p")
+    price.innerHTML=ele.price
+    price.setAttribute("id", "price")
+    let images=document.createElement("img")
+   images.src = ele.thumbnail
+   images.setAttribute("id", "img")
+
+   let card=document.createElement("div")
+   card.innerHTML=card.innerHTML
+   card.setAttribute("id", "card")
+    card.append(title,images,price)
+    document.getElementById("box").append(card)
+    
+})
+}
+
+
+// fetch("https://dummyjson.com/products")
+// .then((res)=>res.json())
+// .then((data)=>uimaker(data.products));
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then((data) => uimaker(data.products))
---
+++ ex3-calculator/tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+class Calc2 extends calculator {
+
+//     constructor (a,b,c){
+//        super (a,b);
+//        this.c = c;
+//     }
+//     add(){
+//         console.log(this.a+this.b+this.c);
+//     }
+//     sum(){
+//         console.log(this.a+this.b+this.c);
+//     }
+//     multi(){
+//         console.log(this.a*this.b*this.c);
+//     }
+//     divide(){
+//         console.log(this.a/this.b/this.c);
+//     }
+    
+   
+// }
